085561560c3b0e80e8c78d4b22bf4fbe78f156ec
sl921203 <100337979+sl921203@users.noreply.github.com>
2022-07-15 19:09:08 +0800

    Update Demo.java
    pull com

--- src/Demo.java
+++ src/Demo.java
@@ -9,5 +9,6 @@ public class Demo {
         System.out.println("hello master test");
         System.out.println("hello hot-fix test");
         System.out.println("hello push test");
+         System.out.println("hello pull test");
     }
 }
